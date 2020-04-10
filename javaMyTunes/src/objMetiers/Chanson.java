4fef531e09fa93035fce77b380b07fed42c2a44c
jim <daftjim1@gmail.com>
2020-04-10 11:02:48 +0200

    MAJ objMetiers Chanson et Playlist

--- javaMyTunes/src/objMetiers/Chanson.java
+++ javaMyTunes/src/objMetiers/Chanson.java
@@ -1,5 +1,176 @@
 package objMetiers;
 
-public class Chanson {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="chanson")
+public class Chanson {
+	
+	@Id //clé primaire
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //auto increment
+	@Column(name="Id")
+	private int id = -1;
+	
+	@Column(name="titre")
+	private String titre = null;
+	
+	@Column(name="nomFichier")
+	private String nomFichier = null;
+	
+	@OneToOne //cardinalité 0,1 dans notre schéma E/A
+	@JoinColumn(name="idAlbum") //clé étrangère sur Album_id
+	private Album album = null;
+	
+	@OneToOne //cardinalité 0,1 dans notre schéma E/A
+	@JoinColumn(name="idArtiste") //clé étrangère sur Artiste_id
+	private Artiste artiste = null;
+	
+	@OneToOne //cardinalité 0,1 dans notre schéma E/A
+	@JoinColumn(name="idGenre") //clé étrangère sur Genre_id
+	private Genre genre = null;
+	
+	@ManyToMany(mappedBy="contient") //pour gérer la relation entre playlist <> chanson
+	private List<Playlist> listePlaylists = new ArrayList<Playlist>();
+
+	public Chanson() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Chanson(String titre, String nomFichier) {
+		super();
+		this.titre = titre;
+		this.nomFichier = nomFichier;
+	}
+	
+	public Chanson(int id, String titre, String nomFichier) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.nomFichier = nomFichier;
+	}
+
+	public Chanson(int id, String titre, String nomFichier, Album album, Artiste artiste, Genre genre,
+			List<Playlist> listePlaylists) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.nomFichier = nomFichier;
+		this.album = album;
+		this.artiste = artiste;
+		this.genre = genre;
+		this.listePlaylists = listePlaylists;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getNomFichier() {
+		return nomFichier;
+	}
+
+	public void setNomFichier(String nomFichier) {
+		this.nomFichier = nomFichier;
+	}
+
+	public Album getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(Album album) {
+		this.album = album;
+	}
+
+	public Artiste getArtiste() {
+		return artiste;
+	}
+
+	public void setArtiste(Artiste artiste) {
+		this.artiste = artiste;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public List<Playlist> getListePlaylists() {
+		return listePlaylists;
+	}
+
+	public void setListePlaylists(List<Playlist> listePlaylists) {
+		this.listePlaylists = listePlaylists;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Chanson))
+			return false;
+		Chanson other = (Chanson) obj;
+		if (album == null) {
+			if (other.album != null)
+				return false;
+		} else if (!album.equals(other.album))
+			return false;
+		if (artiste == null) {
+			if (other.artiste != null)
+				return false;
+		} else if (!artiste.equals(other.artiste))
+			return false;
+		if (genre == null) {
+			if (other.genre != null)
+				return false;
+		} else if (!genre.equals(other.genre))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nomFichier == null) {
+			if (other.nomFichier != null)
+				return false;
+		} else if (!nomFichier.equals(other.nomFichier))
+			return false;
+		if (titre == null) {
+			if (other.titre != null)
+				return false;
+		} else if (!titre.equals(other.titre))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Chanson [id=" + id + ", titre=" + titre + ", nomFichier=" + nomFichier + ", album=" + album
+				+ ", artiste=" + artiste + ", genre=" + genre + "]";
+	}
+	
+	//VOIR POUR AJOUTER METHODE POUR TESTER SI LE TITRE DE LA CHANSON EST VIDE OU NON
 }
--- javaMyTunes/src/objMetiers/Playlist.java
+++ javaMyTunes/src/objMetiers/Playlist.java
@@ -1,5 +1,113 @@
 package objMetiers;
 
-public class Playlist {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Playlist")
+public class Playlist {
+	
+	@Id //clé primaire
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //auto increment
+	@Column(name="Id")
+	private int id = -1;
+	
+	@Column(name="nomPlaylist")
+	private String nom = null;
+	
+	@ManyToMany //pour gérer la relation entre Playlist <> Chanson
+	@JoinTable(name="ContientPlaylist",
+	joinColumns= {
+			@JoinColumn(table="Playlist", name="id", referencedColumnName="idPlaylist")
+	},
+	inverseJoinColumns= {
+			@JoinColumn(table="chanson", name="id", referencedColumnName="idChanson")
+	})
+	private List<Chanson> contient = new ArrayList<Chanson>();
+
+	public Playlist() {
+		super();
+	}
+	
+	public Playlist(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public Playlist(String nom, List<Chanson> contient) {
+		super();
+		this.nom = nom;
+		this.contient = contient;
+	}
+
+	public Playlist(int id, String nom, List<Chanson> contient) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.contient = contient;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public List<Chanson> getContient() {
+		return contient;
+	}
+
+	public void setContient(List<Chanson> contient) {
+		this.contient = contient;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Playlist))
+			return false;
+		Playlist other = (Playlist) obj;
+		if (contient == null) {
+			if (other.contient != null)
+				return false;
+		} else if (!contient.equals(other.contient))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Playlist [id=" + id + ", nom=" + nom + ", contient=" + contient + "]";
+	}
+	
+	//VOIR POUR AJOUTER METHODE POUR TESTER SI LE TITRE DE LA PLAYLIST EST VIDE OU NON
+	
 }
