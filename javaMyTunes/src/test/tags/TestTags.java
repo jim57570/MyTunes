be59fa80102929dd1efbdae72f00312e9ca0bdc4
jim <daftjim1@gmail.com>
2020-04-17 14:15:15 +0200

    MAJ id3 tags lib

--- javaMyTunes/src/test/tags/TestTags.java
+++ javaMyTunes/src/test/tags/TestTags.java
@@ -3,55 +3,31 @@ package test.tags;
 import java.io.File;
 import java.io.IOException;
 
-import org.farng.mp3.MP3File;
-import org.farng.mp3.TagConstant;
-import org.farng.mp3.TagException;
-import org.farng.mp3.TagOptionSingleton;
-import org.farng.mp3.id3.AbstractID3v2;
-import org.farng.mp3.id3.ID3v1;
-import org.farng.mp3.id3.ID3v2_4;
+
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.CannotWriteException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+
+
+
 
 public class TestTags {
 
-	public static void main(String[] args) throws IOException, TagException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException, CannotReadException, org.jaudiotagger.tag.TagException, ReadOnlyFileException, InvalidAudioFrameException, CannotWriteException {
 		
-		//MP3 CONTENANT DEJA DES TAGS
-		File source = new File("/home/jim/Music/02 Dr. Feelgood.mp3");
 		
-		if(source.exists()) {
-			MP3File mp3 = new MP3File(source);
-			//TagOptionSingleton.getInstance().setDefaultSaveMode(TagConstant.MP3_FILE_SAVE_OVERWRITE);
-			if(mp3.hasID3v2Tag()) {
-				System.out.println(mp3.getID3v2Tag());
-				//mp3.getID3v2Tag().setSongTitle("OUI");
-				//mp3.getID3v2Tag().setAlbumTitle("NON");
-				//mp3.save();
-			}
-			else {
-				System.out.println("Ce mp3 n'a pas de tag ID3v2 !");
-			}
-		}
-		else {
-			System.out.println("fichier introuvable !");
-		}
-		
-		/*
-		//MP3 N AYANT PAS DE TAG
-		File source = new File("/home/jim/Music/Beck - Debra.mp3");
-		if(source.exists()) {
-			MP3File mp3 = new MP3File(source);
-			if(mp3.hasID3v2Tag()) {
-				System.out.println(mp3.getID3v2Tag());
-			}
-			else {
-				System.out.println("Ce mp3 n'a pas de tag ID3v2 !");
-			}
-		}
-		else {
-			System.out.println("fichier introuvable !");
-		}*/
-		
+		//TEST JAUDIOTAGGER
+		File source = new File("/home/jim/Music/01 Nappeux.mp3");
+		AudioFile f = AudioFileIO.read(source);
+		Tag tag = f.getTag();
+		System.out.println(tag.getFirst(FieldKey.COMPOSER));
+		tag.setField(FieldKey.TITLE, "OUI");
+		f.commit();
 	}
 
 }
