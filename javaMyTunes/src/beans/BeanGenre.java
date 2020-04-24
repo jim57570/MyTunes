86f84df07e1c8eda3456cf75c1ca5d2d954feefc
jim <daftjim1@gmail.com>
2020-04-24 14:38:07 +0200

    Add listeGenres.xhtml et bean genre

---
+++ javaMyTunes/src/beans/BeanGenre.java
@@ -0,0 +1,14 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import dao.jpa.DaoGenreJPA;
+import objMetiers.Genre;
+
+public class BeanGenre implements Serializable {
+	
+	public List<Genre> getGenres() {
+		return DaoGenreJPA.getInstance().loadAll();
+	}
+}
